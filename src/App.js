bd5b7b48da28a71a275877cff7ed63244cf44b54
polash <johndoe@example.com>
2021-02-07 14:11:23 +0600

    fontEnd end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>25 + 5 Clock</h1>
       </header>
+      <div className="main">
+        <div>
+          <h5>Break Length</h5>
+          <button>-</button>
+          <button>5</button>
+          <button>+</button>
+        </div>
+        <div>
+          <h5>Session Length</h5>
+          <button>-</button>
+          <button>5</button>
+          <button>+</button>
+        </div>
+      </div>
+      <footer>
+        <h3>Session</h3>
+        <span>25:00</span>
+      </footer>
+      <div className="buttons">
+        <button>Start</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
